0914f015b51e835606bcbee79e21b16723e1f553
solene.vandercruyssen <solenevandercruyssen2@gmail.com>
2025-07-22 16:41:42 +0200

    docs: ajout pages

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
+require (__DIR__. "/src/template/header.php");
 require __DIR__ . '/src/routes.php';
+
 ?>
 
 <p>bonjour</p>
+
+
+<?php 
+
+require (__DIR__."/src/template/footer.php");
--- src/config/pdo.php
+++ src/config/pdo.php
@@ -1,7 +1,8 @@
 <?php 
 /**
  * Retourne une instance de connexion PDO pour se connecter à la BDD "boulangerie"
- *
+ * PDO = (PHP Data Object)
+ * Pour se connecter, PDO demande un DSN (Data Source Name) 
  * @return \PDO
  */
 function connexionPDO(): \PDO
@@ -10,6 +11,10 @@ function connexionPDO(): \PDO
     {
         // Je récupère ma configuration :
         $config = require __DIR__."/../config/blogconfig.php";
+        /* 
+        C'est un string contenant toute les informations pour localiser la BDD.
+        * {pilote}:host={adresse};port={port de connexion};dbname={nom de la bdd};charset={charset à utiliser}
+        */
         // Je construit mon DSN :
         $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']};charset={$config['charset']}";
         // Je crée mon instance de PDO :
